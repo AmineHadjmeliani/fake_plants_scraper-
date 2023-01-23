9d4ab40e8c49595627fbce1afdfacf92fa018e7d
Amine HADJ MELIANI <112354395+AmineHadjmeliani@users.noreply.github.com>
2023-01-23 14:35:36 +0100

    Update plantspider.py

--- spiders/plantspider.py
+++ spiders/plantspider.py
@@ -3,26 +3,29 @@ from scrapy.loader import ItemLoader
 from ..items import FakeplantItem 
 
 class PlantspiderSpider(scrapy.Spider):
-    name = 'plantspider'
-    allowed_domains = ['fake-plants.co.uk']
-    start_urls = ['https://www.fake-plants.co.uk/']
+    name = 'plantspider' # spider name
+    allowed_domains = ['fake-plants.co.uk'] # allowed domains to crawl
+    start_urls = ['https://www.fake-plants.co.uk/'] # starting url
 
     def parse(self, response):
+        # extract links for categories
         for link in response.css("li.product-category a::attr(href)"):
             yield response.follow(link.get(), callback=self.parse_cat)
     
     def parse_cat(self, response):
+        # extract links for plants
         for link in response.css("div.astra-shop-thumbnail-wrap a::attr(href)"):
             yield scrapy.Request(link.get(), callback=self.parse_plant)
             
+            # extract links for next pages
             next_page = response.css('a.next.page-numbers::attr(href)').get()
         if next_page :
             yield scrapy.Request(next_page, callback = self.parse_cat)
     
-    
     def parse_plant(self, response):
+        # extract plant information
         il = ItemLoader(item=FakeplantItem(), selector=response)
         il.add_css('title', 'h1.product_title.entry-title')
         il.add_css('category', 'span.posted_in a')
         il.add_css('tags', 'span.tagged_as a')
-        yield il.load_item()
+        yield il.load_item() # yield the extracted item
