3b1109f9c9c1c0646ffb959a77c10b974710d642
Amine HADJ MELIANI <112354395+AmineHadjmeliani@users.noreply.github.com>
2023-01-23 14:30:51 +0100

    Update plantcrawler.py

--- spiders/plantcrawler.py
+++ spiders/plantcrawler.py
@@ -5,18 +5,18 @@ from scrapy.loader import ItemLoader
 from ..items import FakeplantItem 
 
 class PlantcrawlerSpider(CrawlSpider):
-    name = 'plantcrawler'
-    allowed_domains = ['fake-plants.co.uk']
-    start_urls = ['https://www.fake-plants.co.uk/']
-    
-    le_plant = LinkExtractor(restrict_css='div.astra-shop-thumbnail-wrap') 
-    le_next = LinkExtractor(restrict_css='a.next.page-numbers')
-    le_cats = LinkExtractor(restrict_css='li.product-category.product a')
-    
-    rule_plant = Rule(le_plant, callback='parse_item', follow = False)
-    rule_next = Rule(le_next,follow = True)
-    rule_cats = Rule(le_cats,follow = True)
-    
+    name = 'plantcrawler' # spider name
+    allowed_domains = ['fake-plants.co.uk'] # allowed domains to crawl
+    start_urls = ['https://www.fake-plants.co.uk/'] # starting url
+
+    le_plant = LinkExtractor(restrict_css='div.astra-shop-thumbnail-wrap') # extract links for plants
+    le_next = LinkExtractor(restrict_css='a.next.page-numbers') # extract links for next pages
+    le_cats = LinkExtractor(restrict_css='li.product-category.product a') # extract links for categories
+
+    rule_plant = Rule(le_plant, callback='parse_item', follow = False) # follow links for plants and call parse_item function
+    rule_next = Rule(le_next,follow = True) # follow links for next pages
+    rule_cats = Rule(le_cats,follow = True) # follow links for categories
+
     rules = (rule_plant,
              rule_next, 
              rule_cats
@@ -24,7 +24,8 @@ class PlantcrawlerSpider(CrawlSpider):
 
     def parse_item(self, response):
         il = ItemLoader(item=FakeplantItem(), selector=response)
-        il.add_css('title', 'h1.product_title.entry-title')
-        il.add_css('category', 'span.posted_in a')
-        il.add_css('tags', 'span.tagged_as a')
-        yield il.load_item()
+        il.add_css('title', 'h1.product_title.entry-title') # extract plant title
+        il.add_css('category', 'span.posted_in a') # extract plant category
+        il.add_css('tags', 'span.tagged_as a') # extract plant tags
+        yield il.load_item() # yield the extracted item
+
